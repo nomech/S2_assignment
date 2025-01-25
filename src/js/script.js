6abe740d501fcce64516ec6eb44cdaed504ebafe
nomech <mech1803@gmail.com>
2025-01-25 21:50:49 +0100

    Add images, consolidate render files

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,19 @@
-import { render } from "./render.js";
 import { renderNavButtons } from "./renderNavButtons.js";
-import { renderSpecificItem } from "./utils.js";
+//import { renderSpecificItem } from "./utils.js";
+import { renderData } from "./renderData.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   renderNavButtons();
 
   document.addEventListener("click", (event) => {
     if (event.target.classList.contains("button")) {
-      render(event);
+      const id = event.target.dataset.id;
+      const url = event.target.dataset.url;
+      renderData(url, id);
     }
 
-    if (event.target.classList.contains("data-card__name")) {
+    /* if (event.target.classList.contains("data-card__name")) {
       renderSpecificItem(event.target.dataset.url);
-    }
+    } */
   });
 });
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,5 +1,54 @@
 import { getStarWarsData, getSubData } from "./dataFetchers.js";
 
+export const imageLibrary = {
+  people: {
+    1: "./src/assets/images/LukeSkywalker.webp",
+    2: "./src/assets/images/C3PO.webp",
+    3: "./src/assets/images/R2D2.webp",
+    4: "./src/assets/images/darthVader.webp",
+    5: "./src/assets/images/leiaOrgana.webp",
+    6: "./src/assets/images/OwenLars.webp",
+    7: "./src/assets/images/BeruWhitesunLars.webp",
+    8: "./src/assets/images/R5D4.webp",
+    9: "./src/assets/images/BiggsDarklighter.webp",
+    10: "./src/assets/images/obiWanKenobi.webp",
+  },
+  planets: {
+    1: "./src/assets/images/tatooine.webp",
+    2: "./src/assets/images/alderaan.webp",
+    3: "./src/assets/images/yavinIV.webp",
+    4: "./src/assets/images/hoth.webp",
+    5: "./src/assets/images/dagobah.webp",
+    6: "./src/assets/images/bespin.webp",
+    7: "./src/assets/images/endor.webp",
+    8: "./src/assets/images/naboo.webp",
+    9: "./src/assets/images/coruscant.webp",
+    10: "./src/assets/images/kamino.webp",
+  },
+  films: {
+    1: "./src/assets/images/episode4.webp",
+    2: "./src/assets/images/episode5.webp",
+    3: "./src/assets/images/episode6.webp",
+    4: "./src/assets/images/episode1.webp",
+    5: "./src/assets/images/episode2.webp",
+    6: "./src/assets/images/episode3.webp",
+    7: "./src/assets/images/episode7.webp",
+  },
+  vehicles: {
+    4: "./src/assets/images/sandcrawler.webp",
+    6: "./src/assets/images/t16.webp",
+    7: "./src/assets/images/x34.webp",
+    8: "./src/assets/images/tiefighter.webp",
+    14: "./src/assets/images/snowspeeder.webp",
+    16: "./src/assets/images/tiebomber.webp",
+    18: "./src/assets/images/atat.webp",
+    19: "./src/assets/images/atst.webp",
+    20: "./src/assets/images/cloudcar.webp",
+    24: "./src/assets/images/sailbarge.webp",
+  },
+  placeholder: "./src/assets/images/placeholder.webp",
+};
+
 //Sorts the data fetched from the Star Wars API
 export const sortData = (dataArray) => {
   const sortedData = dataArray.sort((a, b) => {
@@ -63,14 +112,12 @@ export const appendSubDataName = async (array) => {
   return list;
 };
 
-export const renderSpecificItem = async (url) => {
+/* export const renderSpecificItem = async (url) => {
   clearContent();
   toggleLoading(true);
   const content = document.querySelector(".content");
   const data = await getStarWarsData(url);
   const dataContainer = document.createElement("div");
-
-  console.log(data)
 
   for (const key in data) {
     if (key === "url") {
@@ -103,4 +150,10 @@ export const renderSpecificItem = async (url) => {
   }
   toggleLoading();
   content.append(dataContainer);
+}; */
+
+export const numberFormatter = (number) => {
+  return !isNaN(parseInt(number))
+    ? parseInt(number).toLocaleString()
+    : "Unknown";
 };
